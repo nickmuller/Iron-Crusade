b575f32baa354b98538b287b500b2a7a2f60635e
Nick <nickmuller@gmail.com>
2025-03-05 15:37:48 +0100

    Auth voor alle paginas en toon ingelogde user

---
+++ Client/Models/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorApp.Client.Models;
+
+public class UserInfo
+{
+    public ClientPrincipal? ClientPrincipal { get; set; }
+}
+
+public class ClientPrincipal
+{
+    public string? IdentityProvider { get; set; }
+    public string? UserId { get; set; }
+    public string? UserDetails { get; set; }
+    public string[]? UserRoles { get; set; }
+}
