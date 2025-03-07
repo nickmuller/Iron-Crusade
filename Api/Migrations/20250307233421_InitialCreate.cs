a5a6219bc5cd92300fe51a6783bd735e521530bb
Nick <nickmuller@gmail.com>
2025-03-08 00:35:34 +0100

    Migrations

---
+++ Api/Migrations/20250307233421_InitialCreate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PersoonlijkeGegevensLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Datum = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Gewicht = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersoonlijkeGegevensLogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkoutLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Categorie = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WorkoutStart = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    WorkoutEind = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkoutLogs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PersoonlijkeGegevensLogs");
+
+            migrationBuilder.DropTable(
+                name: "WorkoutLogs");
+        }
+    }
+}
