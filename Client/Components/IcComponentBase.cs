d079a1bc4664b8d149877f434d2f201372ffa2b1
Nick <nickmuller@gmail.com>
2025-03-12 19:55:02 +0100

    Decimal dropdown component

---
+++ Client/Components/IcComponentBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace IronCrusade.Client.Components;
+
+public abstract class IcComponentBase : ComponentBase
+{
+    [Parameter]
+    public string? Id { get; set; }
+
+    [Parameter]
+    public string? Class { get; set; }
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IDictionary<string, object>? AdditionalAttributes { get; set; }
+}
